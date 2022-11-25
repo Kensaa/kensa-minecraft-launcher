7ddf67b1b033e2ecd96ea7f3f45eb901490eacd8
Kensaa <kensadevs@gmail.com>
2022-11-25 10:25:58 +0100

    started home page

--- launcher/src/pages/Home.tsx
+++ launcher/src/pages/Home.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Button } from 'react-bootstrap'
+
+import authStore from '../stores/auth'
+import configStore from '../stores/config'
 
 export default function Home() {
-  return (
-    <div className='w-100 h-100 d-flex flex-column align-items-center'>
-        Home
-    </div>
-  )
+    const auth = authStore(state => ({...state}))
+    const config = configStore(state => ({...state}))
+
+    return (
+        <div className='w-100 h-100 d-flex flex-column align-items-center p-2'>
+            <div className='h-75'>
+                MEGA FEUR
+            </div>
+        <div className='w-100 d-flex justify-content-center border'>
+            <Button disabled={!auth.connected}>Play</Button>
+        </div>
+        </div>
+    )
 }
