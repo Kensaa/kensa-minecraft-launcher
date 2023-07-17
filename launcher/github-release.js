78b557c9fdff8b3def3b2589e31d54c7f7700f87
Kensaa <kensadevs@gmail.com>
2023-07-18 00:23:03 +0200

    created release helper script

---
+++ launcher/github-release.js
@@ -0,0 +1,62 @@
+const { execSync } = require('child_process')
+const fs = require('fs')
+
+const version = require('./package.json').version
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+function input() {
+    return new Promise(resolve => {
+        readline.question('', resolve)
+    })
+} //build
+//get note from args (parse \n)
+//release
+//delete release folder
+
+;(async () => {
+    console.log('building launcher')
+    execSync('yarn build', { stdio: 'inherit' })
+    let notes
+    while (true) {
+        notes = await askNotes()
+        const addInscruction = await ask(
+            'do you want to append the executables inscruction at the end ?'
+        )
+        if (addInscruction) {
+            notes +=
+                '\n.msi pour windows\n.exe pour la version (a moitié)portable\n.zip pour les autres'
+        }
+        console.log('----------------------------------')
+        console.log(notes)
+
+        if (await ask('is that correct ?')) break
+    }
+    fs.writeFileSync('notes', notes)
+
+    const releaseCommand = `gh release create v${version} -t "v${version}" --notes-file notes "release/Kensa Minecraft Launcher ${version}.exe" "release/Kensa Minecraft Launcher ${version}.msi" "release/kensa-minecraft-launcher-${version}.zip"`
+
+    execSync(releaseCommand)
+    console.log('release successfully created !')
+    fs.rmSync('notes')
+    fs.rmdirSync('release', { recursive: true, force: true })
+})()
+
+async function askNotes() {
+    const lines = []
+    console.log('enter the note for the release:')
+    while (true) {
+        const resp = await input()
+        if (resp === '') break
+        lines.push(resp)
+    }
+    return lines.join('\n')
+}
+
+async function ask(question) {
+    console.log(question)
+    console.log('Y/n ')
+    const resp = await input()
+    return resp === ''
+}
