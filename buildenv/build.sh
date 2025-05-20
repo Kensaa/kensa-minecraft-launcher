46dcee5404c594bbe559896871bc237fcccc8906
Kensaa <54020933+Kensaa@users.noreply.github.com>
2025-05-20 19:01:23 +0200

    retry has selfhosted

--- buildenv/build.sh
+++ buildenv/build.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
-sudo docker buildx build -t docker.kensa.fr/electron-build-image . 
-sudo docker push docker.kensa.fr/electron-build-image
+docker buildx build -t docker.kensa.fr/electron-build-image . 
+docker push docker.kensa.fr/electron-build-image
