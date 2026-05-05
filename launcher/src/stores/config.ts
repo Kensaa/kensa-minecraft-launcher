9678bc1bcfd95eb28c32d7027740781b86c03286
Kensaa <54020933+Kensaa@users.noreply.github.com>
2026-05-06 01:48:55 +0200

    feat: refactor config store to no longer need changes when config type changes

--- launcher/src/stores/config.ts
+++ launcher/src/stores/config.ts
@@ -2,59 +2,39 @@ import { create } from 'zustand'
 import { ipcRenderer } from 'electron'
 import { Config } from '../types'
 
-interface configStore {
-    rootDir: string
-    ram: number
-    servers: string[]
-    closeLauncher: boolean
-    openLogs: boolean
-    showHiddenProfiles: boolean
-    setRootDir: (dir: string) => void
-    setRam: (ram: number) => void
-    setServers: (servers: string[]) => void
-    setCloseLauncher: (closeLauncher: boolean) => void
-    setOpenLogs: (openLogs: boolean) => void
-    setShowHiddenProfiles: (showHiddenProfiles: boolean) => void
-    resetConfig: () => void
+type Setter<T> = (val: T) => void
+type ConfigSetters = {
+    [K in keyof Config as `set${Capitalize<string & K>}`]: Setter<Config[K]>
 }
+type ConfigStore = Config &
+    ConfigSetters & {
+        resetConfig: () => void
+    }
 
-const store = create<configStore>(set => {
+const store = create<ConfigStore>((set, get) => {
     const config = ipcRenderer.sendSync('get-config') as Config
+    const configKeys = Object.keys(config) as (keyof Config)[]
 
+    function setterFactory<K extends keyof ConfigStore>(key: K) {
+        return (val: ConfigStore[K]) => {
+            if (get()[key] !== val) {
+                set({ [key]: val })
+                ipcRenderer.send('set-config', JSON.stringify({ [key]: val }))
+            }
+        }
+    }
+    const setters = Object.fromEntries(
+        configKeys.map(key => {
+            return [
+                `set${key[0].toUpperCase()}${key.substring(1)}`,
+                setterFactory(key)
+            ]
+        })
+    ) as ConfigSetters
+    console.log(setters)
     return {
-        rootDir: config.rootDir,
-        ram: config.ram,
-        servers: config.servers,
-        closeLauncher: config.closeLauncher,
-        openLogs: config.openLogs,
-        showHiddenProfiles: config.showHiddenProfiles,
-        setRootDir: (rootDir: string) => {
-            set({ rootDir })
-            ipcRenderer.send('set-config', JSON.stringify({ rootDir }))
-        },
-        setRam: (ram: number) => {
-            set({ ram })
-            ipcRenderer.send('set-config', JSON.stringify({ ram }))
-        },
-        setServers: (servers: string[]) => {
-            set({ servers })
-            ipcRenderer.send('set-config', JSON.stringify({ servers }))
-        },
-        setCloseLauncher: (closeLauncher: boolean) => {
-            set({ closeLauncher })
-            ipcRenderer.send('set-config', JSON.stringify({ closeLauncher }))
-        },
-        setOpenLogs: (openLogs: boolean) => {
-            set({ openLogs })
-            ipcRenderer.send('set-config', JSON.stringify({ openLogs }))
-        },
-        setShowHiddenProfiles: (showHiddenProfiles: boolean) => {
-            set({ showHiddenProfiles })
-            ipcRenderer.send(
-                'set-config',
-                JSON.stringify({ showHiddenProfiles })
-            )
-        },
+        ...config,
+        ...setters,
         resetConfig: () => {
             ipcRenderer.sendSync('reset-config')
             const newConfig = JSON.parse(ipcRenderer.sendSync('get-config'))
